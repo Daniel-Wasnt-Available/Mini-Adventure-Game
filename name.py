737aff200992f14eccd718a6fbde626fe515436c
Daniel-Wasnt-Available <1wangdan5@hdsb.ca>
2020-10-01 09:43:04 -0400

    renamed file

---
+++ name.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+# Name:        New File Generator (newFile.py)
+# Purpose:     Generates a new file for use in the ICS3U course
+#
+# Author:      Mr. Brooks
+# Created:     13-Sept-2020
+# Updated:     13-Sept-2020
+#-----------------------------------------------------------------------------
+#I think this project deserves a XXXXXX because ...
+#
+#Features Added:
+#   ...
+#   ...
+#   ...
+#-----------------------------------------------------------------------------
